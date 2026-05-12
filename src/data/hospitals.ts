7a7dd7597497e2b538b8848957411c1e327603e9
praveen <praveen991210@gmail.com>
2026-05-12 12:13:02 +0530

    addmorefile

---
+++ src/data/hospitals.ts
@@ -0,0 +1,14 @@
+import type { Hospital } from "@/types";
+
+export const hospitals: Hospital[] = [
+  {
+    id: "11111111-1111-1111-1111-111111111111",
+    name: "Meridian Downtown Hospital",
+    code: "MERIDIAN-DOWNTOWN",
+  },
+  {
+    id: "22222222-2222-2222-2222-222222222222",
+    name: "Meridian West Clinic",
+    code: "MERIDIAN-WEST",
+  },
+];
